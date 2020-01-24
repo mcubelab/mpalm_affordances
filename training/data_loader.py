ad963e05ffbcd745cdc105f88626dd6d62804a9f
anthonysimeonov <asimeono@mit.edu>
2020-01-24 17:25:46 -0500

    initial VAE skeleton

---
+++ training/data_loader.py
@@ -0,0 +1,79 @@
+import torch
+import os
+import random
+import copy
+import numpy as np
+import pickle
+
+
+class DataLoader(object):
+    def __init__(self, data_dir):
+        # path to data?
+        self.data_dir = data_dir
+        self.start_reps = ['pose', 'keypoints', 'pcd']
+
+    def create_random_ordering(self, size=None):
+        self.filenames = []
+
+        for filename in os.listdir(self.data_dir):
+            if filename.endswith('.pkl') and filename != 'metadata.pkl':
+                self.filenames.append(os.path.join(self.data_dir, filename))
+
+        random.shuffle(self.filenames)
+        if size is not None:
+            self.filenames = self.filenames[:size]
+
+    def load_batch(self, start_ind, batch_size, start_rep='pose'):
+        if start_ind + batch_size < len(self.filenames):
+            batch_filenames = self.filenames[start_ind:start_ind+batch_size]
+        else:
+            batch_filenames = self.filenames[start_ind:]
+
+        batch_inputs = []
+        batch_targets = []
+        for i, fname in enumerate(batch_filenames):
+            with open(fname, 'rb') as f:
+                data = pickle.load(f)
+            input_sample, target_sample = \
+                self.load_sample(data, start_rep=start_rep)
+            batch_inputs.append(input_sample)
+            batch_targets.append(target_sample)
+
+        return np.asarray(batch_inputs, dtype=np.float32), np.asarray(batch_targets, dtype=np.float32)
+
+    def load_sample(self, data, start_rep):
+        if start_rep not in self.start_reps:
+            raise ValueError('Start state representation not recognized')
+
+        if start_rep == 'pose':
+            start_sample = data['start']
+            goal_sample = data['goal']
+            input_sample = start_sample + goal_sample
+
+        elif start_rep == 'keypoints':
+            # have to take care of what order?
+            start_sample = data['keypoints_start']
+            goal_sample = data['keypoints_goal']
+            input_sample = np.hstack(
+                (start_sample.flatten(), goal_sample.flatten())).tolist()
+        elif start_rep == 'pcd':
+            start_sample = np.concatenate(data['obs']['pcd_pts']).tolist()
+            goal_sample = data['goal']
+            # goal_sample = data['keypoints_goal']
+
+            input_sample = start_sample + goal_sample
+
+        target_sample = data['contact_obj_frame']
+        return input_sample, target_sample
+
+def main():
+    data_dir = '/home/anthony/repos/research/mpalm_affordances/catkin_ws/src/primitives/data/pull/face_ind_large_0_fixed'
+    dataloader = DataLoader(data_dir=data_dir)
+    dataloader.create_random_ordering()
+
+    batch = dataloader.load_batch(0, 100, start_rep='pose')
+    from IPython import embed
+    embed()
+
+if __name__ == "__main__":
+    main()
---
+++ training/model.py
@@ -0,0 +1,179 @@
+import argparse
+import torch
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
+from data_loader import DataLoader
+
+
+class Encoder(nn.Module):
+
+    def __init__(self,
+                 in_dim,
+                 mu_out_dim,
+                 logvar_out_dim,
+                 mu_head_in,
+                 logvar_head_in,
+                 hidden_dim=128):
+        super(Encoder, self).__init__()
+        self.hidden_layers = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU()
+        )
+        self.mu_head = nn.Sequential(
+            nn.Linear(hidden_dim, mu_out_dim)
+        )
+        self.logvar_head = nn.Sequential(
+            nn.Linear(hidden_dim, logvar_out_dim)
+        )
+
+    def forward(self, x):
+        h = self.hidden_layers(x)
+        mu_out = self.mu_head(h)
+        logvar_out = self.logvar_head(h)
+        return mu_out, logvar_out
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, in_dim, out_dim, hidden_dim=128):
+        super(Decoder, self).__init__()
+        self.decoder = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, out_dim)
+        )
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class VAE():
+    def __init__(self, in_dim, out_dim, latent_dim):
+        self.encoder = Encoder(
+            in_dim=in_dim,
+            mu_out_dim=latent_dim,
+            logvar_out_dim=latent_dim,
+            mu_head_in=64,
+            logvar_head_in=64)
+        self.decoder = Decoder(
+            in_dim=latent_dim,
+            out_dim=out_dim)
+        self.mse = nn.MSELoss()
+
+        params = list(self.encoder.parameters()) + \
+            list(self.decoder.parameters())
+        self.optimizer = optim.Adam(params)
+
+    def encode(self, x):
+        z_mu, z_logvar = self.encoder(x)
+        z = self.reparameterize(z_mu, z_logvar)
+        return z, z_mu, z_logvar
+
+    def decode(self, z):
+        recon_mu = self.decoder(z)
+        return recon_mu
+
+    def forward(self, x):
+        z, z_mu, z_logvar = self.encode(x)
+        recon_mu = self.decode(z)
+        return z, recon_mu, z_mu, z_logvar
+
+    def reparameterize(self, mu, logvar):
+        std_dev = torch.exp((0.5) * logvar)
+        eps = torch.normal(mu, std_dev)
+        z = mu + std_dev * eps
+        return z
+
+    def kl_loss(self, mu, sigma):
+        kl_loss = 0.5 * torch.sum(1 + torch.log(torch.pow(sigma, 2)) -
+                                  torch.pow(sigma, 2) - torch.pow(mu, 2))
+        return kl_loss
+
+    def recon_loss(self, prediction, target):
+        return self.mse(prediction, target)
+
+    def total_loss(self, prediction, target, mu, logvar):
+        sigma = torch.exp(0.5 * logvar)
+        kl_loss = self.kl_loss(mu, sigma)
+        recon_loss = self.recon_loss(prediction, target)
+        total_loss = kl_loss + recon_loss
+        return total_loss
+
+
+def to_var(x, volatile=False):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x, volatile=volatile)
+
+
+def main(args):
+    # reconstruction loss
+    # (1 / L) * torch.sum(torch.log())
+    # mse?
+    batch_size = args.batch_size
+    dataset_size = args.total_data_size
+
+    vae = VAE(
+        args.input_dimension,
+        args.output_dimension,
+        args.latent_dimension
+    )
+
+    if torch.cuda.is_available():
+        vae.encoder.cuda()
+        vae.decoder.cuda()
+
+    data_dir = args.data_dir
+    data_loader = DataLoader(data_dir=data_dir)
+
+    data_loader.create_random_ordering(size=dataset_size)
+
+    total_loss = 0
+    for epoch in range(args.num_epochs):
+        print('Epoch: ' + str(epoch))
+        running_total_loss = 0
+        for i in range(0, dataset_size, batch_size):
+            vae.encoder.zero_grad()
+            vae.decoder.zero_grad()
+
+            input_batch, target_batch = data_loader.load_batch(i, batch_size)
+            input_batch = to_var(torch.from_numpy(input_batch))
+            target_batch = to_var(torch.from_numpy(target_batch))
+
+            z, recon_mu, z_mu, z_logvar = vae.forward(input_batch)
+            # target = torch.normal(z_mu)
+            output = recon_mu
+
+            loss = vae.total_loss(output, target_batch, z_mu, z_logvar)
+            loss.backward()
+            vae.optimizer.step()
+
+            running_total_loss = running_total_loss + loss.data
+            print(" -- running loss: ")
+            print(running_total_loss)
+        print(' --avgerage loss: ')
+        print(running_total_loss/(dataset_size/batch_size))
+        total_loss = total_loss + running_total_loss
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--batch_size', type=int,
+                        default=100)
+    parser.add_argument('--num_epochs', type=int,
+                        default=1)
+    parser.add_argument('--total_data_size', type=int,
+                        default=1000)
+    parser.add_argument('--data_dir', type=str,
+                        default='/home/anthony/repos/research/mpalm_affordances/catkin_ws/src/primitives/data/pull/face_ind_large_0_fixed')
+    parser.add_argument('--input_dimension', type=int,
+                        default=14)
+    parser.add_argument('--latent_dimension', type=int,
+                        default=6)
+    parser.add_argument('--output_dimension', type=int,
+                        default=7)
+
+    args = parser.parse_args()
+    main(args)
