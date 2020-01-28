skeleton for vanilla AE, corrected rotation loss function with proper quaternion normalization

training/model.py
@@ -49,7 +49,7 @@ class Decoder(nn.Module):
         self.decoder = nn.Sequential(
             nn.Linear(in_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, out_dim)
-        )       
+        )
 
     def forward(self, x):
         return self.decoder(x)
@@ -71,7 +71,8 @@ class VAE():
         params = list(self.encoder.parameters()) +  \
             list(self.decoder.parameters())
         self.optimizer = optim.Adam(params, lr=lr)
-        self.beta = 0.001
+#        self.beta = 0.001
+        self.beta = 0.0
 
     def encode(self, x):
         z_mu, z_logvar = self.encoder(x)
@@ -103,17 +104,22 @@ class VAE():
         return kl_loss_mean
 
     def rotation_loss(self, prediction, target):
-        # scalar_prod = torch.mm(prediction, target)
-        # scalar_prod = torch.sum(prediction*target, axis=1)
-        scalar_prod = torch.sum((prediction/torch.norm(prediction)) * target, axis=1)
-        # angle = 2*torch.pow(scalar_prod, 2) - 1
-        # angle = 2*torch.acos(scalar_prod)
+        prediction_norms = torch.norm(prediction, p=2, dim=1)
+        normalized_prediction = torch.cuda.FloatTensor(prediction.shape).fill_(0)
+        for i in range(normalized_prediction.shape[0]):
+            normalized_prediction[i, :] = prediction[i, :]/prediction_norms[i]
+        # scalar_prod = torch.sum((prediction/torch.norm(prediction, p=2, dim=1)) * target, axis=1)
+        scalar_prod = torch.sum(normalized_prediction * target, axis=1)
         dist = 1 - torch.pow(scalar_prod, 2)
-        norm_loss = self.beta*torch.pow((1 - torch.norm(prediction)), 2)
+
+        norm_loss = self.beta*torch.pow((1 - torch.norm(prediction, p=2, dim=1)), 2)
+
         rotation_loss = dist + norm_loss
-        # return rotation_loss
+
         mean_rotation_loss = torch.mean(rotation_loss)
+        # mse_rotation_loss = self.mse(prediction, target)
         return mean_rotation_loss
+        # return mse_rotation_loss
 
     def recon_loss(self, prediction, target):
         pos_prediction = prediction[:, :3]
@@ -132,6 +138,71 @@ class VAE():
         recon_loss = self.recon_loss(prediction, target)
         total_loss = kl_loss + recon_loss
         return total_loss
+
+
+class AE():
+    def __init__(self, in_dim, out_dim, latent_dim, lr):
+        self.encoder = Encoder(
+            in_dim=in_dim,
+            mu_out_dim=latent_dim,
+            logvar_out_dim=latent_dim,
+            mu_head_in=64,
+            logvar_head_in=64)
+        self.decoder = Decoder(
+            in_dim=latent_dim,
+            out_dim=out_dim)
+        self.mse = nn.MSELoss(reduction='mean')
+
+        params = list(self.encoder.parameters()) +  \
+            list(self.decoder.parameters())
+        self.optimizer = optim.Adam(params, lr=lr)
+#        self.beta = 0.001
+        self.beta = 0.0
+
+    def encode(self, x):
+        z_mu, z_logvar = self.encoder(x)
+        return z_mu, z_logvar
+
+    def decode(self, z):
+        recon_mu = self.decoder(z)
+        return recon_mu
+
+    def forward(self, x):
+        z_mu, z_logvar = self.encode(x)
+        recon_mu = self.decode(z_mu)
+        return recon_mu, z_mu, z_logvar
+
+    def rotation_loss(self, prediction, target):
+        prediction_norms = torch.norm(prediction, p=2, dim=1)
+        normalized_prediction = torch.cuda.FloatTensor(prediction.shape).fill_(0)
+        for i in range(normalized_prediction.shape[0]):
+            normalized_prediction[i, :] = prediction[i, :]/prediction_norms[i]
+        # scalar_prod = torch.sum((prediction/torch.norm(prediction, p=2, dim=1)) * target, axis=1)
+        scalar_prod = torch.sum(normalized_prediction * target, axis=1)
+        dist = 1 - torch.pow(scalar_prod, 2)
+
+        norm_loss = self.beta*torch.pow((1 - torch.norm(prediction, p=2, dim=1)), 2)
+
+        rotation_loss = dist + norm_loss
+
+        mean_rotation_loss = torch.mean(rotation_loss)
+        # mse_rotation_loss = self.mse(prediction, target)
+        return mean_rotation_loss
+        # return mse_rotation_loss
+
+    def recon_loss(self, prediction, target):
+        pos_prediction = prediction[:, :3]
+        pos_target = target[:, :3]
+        pos_loss = self.mse(pos_prediction, pos_target)
+
+        ori_prediction = prediction[:, 3:]
+        ori_target = target[:, 3:]
+        ori_loss = self.rotation_loss(ori_prediction, ori_target)
+        return pos_loss + ori_loss
+
+    def total_loss(self, prediction, target):
+        recon_loss = self.recon_loss(prediction, target)
+        return recon_loss
 
 
 def to_var(x, volatile=False):

training/train_vae.py
@@ -15,8 +15,10 @@ def main(args):
     batch_size = args.batch_size
     dataset_size = args.total_data_size
 
-    torch_seed = np.random.randint(low=0, high=1000)
-    np_seed = np.random.randint(low=0, high=1000)
+    # torch_seed = np.random.randint(low=0, high=1000)
+    # np_seed = np.random.randint(low=0, high=1000)
+    torch_seed = 0
+    np_seed = 0
 
     torch.manual_seed(torch_seed)
     np.random.seed(np_seed)
@@ -41,8 +43,8 @@ def main(args):
             trained_model_path,
             args.model_name+'_epoch_%d.pt' % args.start_epoch)
         torch_seed, np_seed = load_seed(fname)
-        load_net_state(vae)
-        load_opt_state(vae)
+        load_net_state(vae, fname)
+        load_opt_state(vae, fname)
         torch.manual_seed(torch_seed)
         np.random.seed(np_seed)
 
@@ -58,6 +60,10 @@ def main(args):
     for epoch in range(args.start_epoch, args.start_epoch+args.num_epochs):
         print('Epoch: ' + str(epoch))
         epoch_total_loss = 0
+        epoch_kl_loss = 0
+        epoch_pos_loss = 0
+        epoch_ori_loss = 0
+        epoch_recon_loss = 0
         for i in range(0, dataset_size, batch_size):
             vae.optimizer.zero_grad()
 
@@ -70,23 +76,38 @@ def main(args):
             output = recon_mu
 
             kl_loss = vae.kl_loss(z_mu, z_logvar)
-            recon_loss = vae.recon_loss(output, target_batch)
+            pos_loss = vae.mse(output[:, :3], target_batch[:, :3])
+            ori_loss = vae.rotation_loss(output[:, 3:], target_batch[:, 3:])
+            recon_loss = pos_loss + ori_loss
+            # recon_loss = vae.recon_loss(output, target_batch)
             # loss = vae.total_loss(output, target_batch, z_mu, z_logvar)
             loss = kl_loss + recon_loss
             loss.backward()
             vae.optimizer.step()
 
             epoch_total_loss = epoch_total_loss + loss.data
+            epoch_kl_loss = epoch_kl_loss + kl_loss.data
+            epoch_pos_loss = epoch_pos_loss + pos_loss.data
+            epoch_ori_loss = epoch_ori_loss + ori_loss.data
+            epoch_recon_loss = epoch_recon_loss + recon_loss.data
             if (i/batch_size) % args.batch_freq == 0:
-                print('Train Epoch: %d [%d/%d (%f)]\tLoss: %f\tKL: %f\tRecon: %f' % (
+                print('Train Epoch: %d [%d/%d (%f)]\tLoss: %f\tKL: %f\tPos: %f\t Ori: %f' % (
                        epoch, i, dataset_size,
                        100.0 * i / dataset_size/batch_size,
                        loss.item(),
                        kl_loss.item(),
-                       recon_loss.item()))
+                       pos_loss.item(),
+                       ori_loss.item()))
         print(' --avgerage loss: ')
         print(epoch_total_loss/(dataset_size/batch_size))
-        total_loss.append(epoch_total_loss/(dataset_size/batch_size))
+        loss_dict = {
+            'epoch_total': epoch_total_loss/(dataset_size/batch_size),
+            'epoch_kl': epoch_kl_loss/(dataset_size/batch_size),
+            'epoch_pos': epoch_pos_loss/(dataset_size/batch_size),
+            'epoch_ori': epoch_ori_loss/(dataset_size/batch_size),
+            'epoch_recon': epoch_recon_loss/(dataset_size/batch_size)
+        }
+        total_loss.append(loss_dict)
 
         if epoch % args.save_freq == 0:
             print('\n--Saving model\n')