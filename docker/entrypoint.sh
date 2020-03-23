4bd9b7bba4fc7ee7335909ff835e72c805709ee2
anthonysimeonov <asimeono@mit.edu>
2020-03-23 17:07:34 -0400

    all in one entrypoint script for docker run

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 # setup ros environment
 source "$HOME/catkin_ws/devel/setup.bash"
+./workspace.sh
+
+source "$HOME/catkin_ws/devel/setup.bash"
+roslaunch config yumi_moveit.launch
 
 eval "bash"
 
