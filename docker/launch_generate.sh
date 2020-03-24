9228b8708053a38bd68bcab9f1d14e9769bbe4c9
Anthony Simeonov <asimeono@vision02.csail.mit.edu>
2020-03-24 19:51:08 -0400

    sudo in data gen launch shell script

--- docker/launch_generate.sh
+++ docker/launch_generate.sh
@@ -2,8 +2,8 @@
 
 base_name="$1"
 echo "Launching data gen in containers matching name: $base_name"
-for a in `sudo docker ps --filter "name=data-gen" --format "{{.Names}}"`
+for a in `sudo docker ps --filter "name=$base_name" --format "{{.Names}}"`
 do
 	echo $a
-	docker exec -d -w /home/asimeono/catkin_ws/src/primitives $a ./generate.sh
+	sudo docker exec -d -w /home/asimeono/catkin_ws/src/primitives $a ./generate.sh
 done
