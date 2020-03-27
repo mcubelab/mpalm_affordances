ac0392be19c6a5fffd541fc5bad7ca16516c19b3
anthonysimeonov <asimeono@mit.edu>
2020-03-26 21:44:23 -0400

    options in docker for root user or local user

--- docker/run_image.bash
+++ docker/run_image.bash
@@ -12,7 +12,9 @@
 # fi
 
 # IMAGE=mpalm-dev-cpu
-IMAGE=mpalm-dev-gpu
+# IMAGE=mpalm-dev-gpu
+IMAGE=anthonysimeonov/mpalm-dev-gpu:0.1.2
+RUN_ARGS="--runtime=nvidia"
 
 XAUTH=/tmp/.docker.xauth
 if [ ! -f $XAUTH ]
@@ -33,10 +35,23 @@ docker run -it \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
-    --volume="$PWD/../catkin_ws/src/:/$HOME/catkin_ws/src/" \
+    --volume="$PWD/../catkin_ws/src/:/root/catkin_ws/src/" \
     --volume="$PWD/workspace.sh:/workspace.sh" \
-    --volume="/home/anthony/repos/research/airobot:$HOME/airobot" \
-    --volume="$PWD/../training/:/$HOME/training/" \
-    --user="anthony" \
+    --volume="/home/anthony/repos/research/airobot:/airobot" \
+    --volume="$PWD/../training/:/root/training/" \
     ${RUN_ARGS} \
     ${IMAGE}
+
+# docker run -it \
+#     --env="DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#     --env="XAUTHORITY=$XAUTH" \
+#     --volume="$XAUTH:$XAUTH" \
+#     --volume="$PWD/../catkin_ws/src/:/$HOME/catkin_ws/src/" \
+#     --volume="$PWD/workspace.sh:/workspace.sh" \
+#     --volume="/home/anthony/repos/research/airobot:$HOME/airobot" \
+#     --volume="$PWD/../training/:/$HOME/training/" \
+#     --user="anthony" \
+#     ${RUN_ARGS} \
+#     ${IMAGE}
