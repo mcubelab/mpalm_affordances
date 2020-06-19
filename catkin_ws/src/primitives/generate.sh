4c8af1b86e3b887ccc70cbedf81328089180f8a4
anthonysimeonov <asimeono@mit.edu>
2020-06-19 12:56:13 -0400

    update generate shell script to reflect master, and line by line argparse style

--- catkin_ws/src/primitives/generate.sh
+++ catkin_ws/src/primitives/generate.sh
@@ -1,3 +1,21 @@
-python generate_data.py --object --primitive grasp --num_trials 10000  \
-    --experiment_name debug --sim_step_repeat 20 --np_seed 2 -v \
-    --num_obj_samples 3 --multi 
+#!/bin/bash
+
+source ~/catkin_ws/devel/setup.bash
+export CODE_BASE=$HOME
+
+# python generate_data.py --object --primitive grasp --num_trials 10000 \
+#     --experiment_name grasping_multi_diverse_0 --sim_step_repeat 20 --np_seed 413 \
+#     --num_obj_samples 500 --num_blocks 40 --multi --save_data
+
+python generate_data.py --object \
+    --primitive pull \
+    --experiment_name pulling_multi_diverse_1 \
+    --sim_step_repeat 20 \
+    --np_seed 197 \
+    --num_obj_samples 20 \
+    --num_blocks 40 \
+    --start_trials 15 \
+    --start_success 20 \
+    --multi \
+    -re \
+    --save_data \
