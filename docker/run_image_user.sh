c1859607d45f36d0cadb7066b6d7fa7ebb05716c
Anthony Simeonov <asimeono@visiongpu36.csail.mit.edu>
2020-09-30 11:08:07 -0400

    only mount minimal necessary resources in docker run command

--- docker/run_image_user.sh
+++ docker/run_image_user.sh
@@ -2,9 +2,7 @@ IMAGE=$USER-mpalm-dev-pytorch-geom
 USERNAME=$USER
 sudo docker run -it \
     --volume="$PWD/../catkin_ws/src/:/home/${USERNAME}/catkin_ws/src/" \
-    --volume="$PWD/workspace.sh:/workspace.sh" \
     --volume="$PWD/../training/:/home/${USERNAME}/training/" \
-    --volume="$PWD/../../airobot/:/home/${USERNAME}/airobot/" \
     --user="${USERNAME}" \
     --gpus all \
     ${IMAGE}
